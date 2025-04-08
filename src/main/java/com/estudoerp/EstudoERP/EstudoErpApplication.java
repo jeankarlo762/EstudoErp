d85b2fb5e62848f6462c4290dbb025293817235c
Jeankarlo762 <jeankarlomaykessler123@gmail.comgit config --global user.email jeankarlomaykessler123@gmail.com>
2025-04-08 08:03:05 -0300

    primeiro commit

---
+++ src/main/java/com/estudoerp/EstudoERP/EstudoErpApplication.java
@@ -0,0 +1,13 @@
+package com.estudoerp.EstudoERP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoErpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudoerp/EstudoERP/EstudoErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudoerp.EstudoERP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
